2ebd7f9834f2f7e25ffd61b4ff0eb012b1af9b77
CristhianGalvez <galvezcristhian05@gmail.com>
2026-03-29 15:07:02 -0600

    feat: Component Andmin, Gestion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { GestionComponent } from './components/admin/gestion/gestion.component'; // <--- Verifica esta ruta
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'home', component: HomeComponent },
-  { path: '**', redirectTo: '' } // Redirigir cualquier otra ruta al login
+  { path: 'admin', component: AdminComponent },
+  { path: 'admin/gestion', component: GestionComponent }, // Nueva ruta para la gestión activa
+  { path: '**', redirectTo: '' }
 ];
