312b61bb5faa11b45daf5095c400c0683c2d73bb
AdrianJMendez <esusjadrian@gmail.com>
2026-03-29 21:58:43 -0600

    adding guards to admin side

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { GestionComponent } from './components/admin/gestion/gestion.component'; // <--- Verifica esta ruta
+import { AuthGuard } from './services/auth.guard';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'admin', component: AdminComponent },
-  { path: 'admin/gestion', component: GestionComponent }, // Nueva ruta para la gestión activa
+  { path: 'admin', component: AdminComponent, canActivate: [AuthGuard] },
+  { path: 'admin/gestion', component: GestionComponent, canActivate: [AuthGuard] }, // Nueva ruta para la gestión activa
   { path: '**', redirectTo: '' }
 ];
